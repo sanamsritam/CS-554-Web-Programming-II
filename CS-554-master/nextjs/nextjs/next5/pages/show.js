d9123835b366687addc2d33b6e3f62c4fc6c1da3
Sanam Sritam Jena <sanamsritam@gmail.com>
2020-01-27 23:53:08 -0500

    Added Professors lecture codes

---
+++ CS-554-master/nextjs/nextjs/next5/pages/show.js
@@ -0,0 +1,13 @@
+import Layout from '../components/MyLayout';
+import { useRouter } from 'next/router';
+
+const Show = () => {
+	const router = useRouter();
+	return (
+		<Layout>
+			<h1>Show details - {router.query.id}</h1>
+		</Layout>
+	);
+};
+
+export default Show;
---
+++ CS-554-master/nextjs/nextjs/next8/pages/index.js
@@ -0,0 +1,13 @@
+import Layout from '../components/MyLayout';
+const Index = () => (
+	<Layout>
+		<h1>Welcome to the TV Maze API Next.js Demo</h1>
+		<style jsx>{`
+			h1 {
+				color: #1e8678;
+			}
+		`}</style>
+	</Layout>
+);
+
+export default Index;
